c953b637c9d6133c4c30514e582a63acf55b2a9a
EuCaue <caueadriano.castro@gmail.com>
2023-12-14 12:12:31 -0300

    refactor(DynamicIcon): better way to find icons

--- src/components/DynamicIcon.tsx
+++ src/components/DynamicIcon.tsx
@@ -1,26 +1,44 @@
 'use client';
 
 import { TablerIconsProps } from '@tabler/icons-react';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 
-export default function DynamicIcon({
-  name,
-  ICON_SIZE
-}: {
+type PropType = {
   name: string;
   ICON_SIZE: number;
-}) {
+};
+
+export default function DynamicIcon({ name, ICON_SIZE }: PropType) {
   const [Icon, setIcon] = useState<React.FC<TablerIconsProps> | null>(null);
+  let foundIcon = useRef<boolean>(false);
+
+  const setSelectedIcon = useCallback((icon: string, iconModule: any) => {
+    const selectedIcon: (props: TablerIconsProps) => JSX.Element =
+      //  FIX: fix this type later @ts-ignore
+      iconModule[`${icon}`];
+    setIcon(selectedIcon);
+  }, []);
 
   useEffect(() => {
     const importIcon = async () => {
       const iconModule = await import('@tabler/icons-react');
-      const selectedIcon: (props: TablerIconsProps) => JSX.Element =
-        //  FIX: fix this type later
-        // @ts-ignore
-        iconModule[`IconBrand${name}`];
-      setIcon(selectedIcon);
-      // console.log('ICON', selectedIcon);
+      for (const icon in iconModule) {
+        if (icon.toLowerCase().includes(name.toLowerCase())) {
+          setSelectedIcon(icon, iconModule);
+          console.log('Icon n', Icon);
+          foundIcon.current = true;
+          break;
+        }
+      }
+      if (name === 'tailwindcss' && foundIcon.current === false) {
+        setIcon(iconModule.IconBrandTailwind);
+        return;
+      }
+
+      if (foundIcon.current === false) {
+        setIcon(iconModule.IconIconsOff);
+        return;
+      }
     };
     importIcon();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
@@ -28,6 +46,7 @@ export default function DynamicIcon({
   if (!Icon) {
     return <></>;
   }
+
   return (
     <Icon
       size={ICON_SIZE}
